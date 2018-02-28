dba47a027d299d81e54f9b29e8031fab6875955b
wyrover <wyrover@gmail.com>
2018-02-28 12:55:57 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,18 @@
+set PATH=C:\msys64\bin;%PATH%
+set PATH=C:\msys64\usr\bin;%PATH%
+set PATH=C:\msys64\mingw64\bin;%PATH%
+
+set CC=x86_64-w64-mingw32-gcc
+
+REM echo useful info
+bash --login -c "$CC -v"
+
+REM appveyor msys configure workaround "exec 0</dev/null"
+REM bash --login -c "cd `cygpath '%CD%'`; exec 0</dev/null; ./autoconf.sh"
+REM bash --login -c "cd `cygpath '%CD%'`; make"
+
+
+
 cd c:\projects\3rdparty-AppVeyor\
 cd src\helloworld
 cl main.cpp
