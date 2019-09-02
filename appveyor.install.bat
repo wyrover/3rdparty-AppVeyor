4a5261044f1c1dab4a8e695fffe63e20c5e7d2b0
wyrover <wyrover@gmail.com>
2019-09-02 18:58:52 +0800

    * appveyor.install.bat

--- appveyor.install.bat
+++ appveyor.install.bat
@@ -13,40 +13,11 @@ dir /a:d /b "C:\Program Files"
 
 set ROOT_DIRECTORY=%CD%
 cd %ROOT_DIRECTORY%
-set BOOST_ROOT=C:\Libraries\boost_1_60_0
-set BOOST_BUILD_PATH=%BOOST_ROOT%\tools\build
 
-set PATH=C:\msys64\bin;%PATH%
-set PATH=C:\msys64\usr\bin;%PATH%
-set PATH=C:\msys64\mingw64\bin;%PATH%
-
-REM echo useful info
-bash --login -c "env"
-bash --login -c "echo $PATH"
-bash --login -c "pacman -Q"
-
-rem bash --login -c "pacman -S --noconfirm python"
-
-rem bash --login -c "curl https://bootstrap.pypa.io/get-pip.py | python"
-rem bash --login -c "pip install cram"
-
-set PATH=%premake_dir%;%PATH%;%BOOST_BUILD_PATH%\src\engine\bin.ntx86
 
 git submodule update --init --recursive
 
 
-if [%PLATFORM%] == [x86] call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86
-if [%PLATFORM%] == [x86] set BOOST_LIBRARYDIR=%BOOST_LIBRARYDIR_WIN32%
-if [%PLATFORM%] == [x86] set CMAKE_GENERATOR=Visual Studio 14 2015    
-    
-
-
-if [%PLATFORM%] == [x64] call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" amd64
-if [%PLATFORM%] == [x64] set BOOST_LIBRARYDIR=%BOOST_LIBRARYDIR_WIN64%   
-if [%PLATFORM%] == [x64] set CMAKE_GENERATOR=Visual Studio 14 2015 Win64   
-
-
-rem wget https://github.com/mlocati/gettext-iconv-windows/releases/download/v0.19.8.1-v1.14/gettext0.19.8.1-iconv1.14-shared-32.zip
     
 
 cmake --version
@@ -54,12 +25,6 @@ msbuild /version
 which perl.exe
 which 7z.exe
 
-echo %BOOST_LIBRARYDIR%
-set PATH=%QT5%\bin;%PATH%
-
-
-if exist "C:\Program Files (x86)\Microsoft SDKs\Windows Kits" ( dir "C:\Program Files (x86)\Microsoft SDKs\Windows Kits" )
-if exist "C:\Program Files\Microsoft SDKs\Windows" ( dir "C:\Program Files\Microsoft SDKs\Windows" )
 
 git config --global core.longpaths true
 
@@ -71,44 +36,12 @@ dir C:\depot_tools
 
 set PATH=C:\depot_tools;%PATH%
 
+echo "build........................."
 
 
+cd ..
+md electron-gn && cd electron-gn
+gclient config --name "src/electron" --unmanaged https://github.com/electron/electron
+gclient sync --with_branch_heads --with_tags
 
 
-
-rem 7z.exe a -t7z -r qt_5_7_1_msvc2015_64.7z C:\Qt\5.7\msvc2015_64\*
-rem appveyor PushArtifact qt_5_7_1_msvc2015_64.7z
-
-
-rem 7z.exe a -t7z -r qt_5_7_1_msvc2015.7z C:\Qt\5.7\msvc2015\*
-rem appveyor PushArtifact qt_5_7_1_msvc2015.7z
-
-
-
-rem 7z.exe a -t7z -r directx_sdk_june_2010.7z "C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\*"
-rem appveyor PushArtifact directx_sdk_june_2010.7z
-
-rem 7z.exe a -t7z -r cygwin.7z "C:\cygwin\*"
-rem appveyor PushArtifact cygwin.7z
-
-rem 7z.exe a -t7z -r msys64.7z "C:\msys64\*"
-rem appveyor PushArtifact msys64.7z
-
-rem 7z.exe a -t7z -r mingw-w64.7z "C:\mingw-w64\*"
-rem appveyor PushArtifact mingw-w64.7z
-
-rem 7z.exe a -t7z -r MinGW.7z "C:\MinGW\*"
-rem appveyor PushArtifact MinGW.7z
-
-
-
-:install_ninja
-
-rem mkdir C:\projects\deps
-rem set NINJA_URL="https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-win.zip"
-
-rem wget %NINJA_URL% -o ninja.zip
-rem 7z x ninja.zip -oC:\projects\deps\ninja > nul
-rem set PATH=C:\projects\deps\ninja;%PATH%
-
-rem ninja --version
--- build.bat
+++
@@ -1,7 +0,0 @@
-echo "build........................."
-
-
-cd ..
-md electron-gn && cd electron-gn
-gclient config --name "src/electron" --unmanaged https://github.com/electron/electron
-gclient sync --with_branch_heads --with_tags
